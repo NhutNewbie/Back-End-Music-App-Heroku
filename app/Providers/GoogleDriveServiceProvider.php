f073157392c9c0bd242ab00d44b1b6a10fd89c5b
NhutNewbie <nhnhut.18it4@sict.udn.vn>
2020-05-07 23:05:48 +0700

    edit

--- app/Providers/GoogleDriveServiceProvider.php
+++ app/Providers/GoogleDriveServiceProvider.php
@@ -26,20 +26,27 @@ class GoogleDriveServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // $client = new \Google_Client;
-        // dd($client);
         \Storage::extend("google", function($app, $config) {
-            dd($config['folderId']);
-            // $client = new \Google_Client;
-            // $client->setClientId('217370947719-c4704v7st4df9roftv8et14o3tv27klh.apps.googleusercontent.com');
-            // $client->setClientSecret('NY7--o2sVfRDRQFEQOm6XPFk');
-            // $client->refreshToken('1//04lh7Wrq7AIfICgYIARAAGAQSNwF-L9IrFZMq7f_9x6TSTIxs5wslbjy65HYOKDAlq6XuJkgzBXGgE5pu-lhi8jyrNCjLOpuVArc');
-            // $client->setAccessType('offline');
-            // $client->setApprovalPrompt('force');
-            // $service = new \Google_Service_Drive($client);
-            // $adapter = new GoogleDriveAdapter($service, '1zl3DzbRCPNbLXu2uN_MEWL1PHB1qWMSK');
+            $client = new \Google_Client;
+            $client->setClientId(config('values.GOOGLE_CLIENT_ID'));
+            $client->setClientSecret(config('values.GOOGLE_CLIENT_SECRET'));
+            $client->refreshToken(config('values.GOOGLE_REFRESH_TOKEN'));
+            $client->setAccessType('offline');
+            $client->setApprovalPrompt('force');
+            $service = new \Google_Service_Drive($client);
+            $adapter = new GoogleDriveAdapter($service, config('values.GOOGLE_DRIVE_FOLDER_ID'));
+            return new Filesystem($adapter);
 
-            // return new Filesystem($adapter);
+        //     // $client = new \Google_Client;
+        //     // $client->setClientId('217370947719-c4704v7st4df9roftv8et14o3tv27klh.apps.googleusercontent.com');
+        //     // $client->setClientSecret('NY7--o2sVfRDRQFEQOm6XPFk');
+        //     // $client->refreshToken('1//04lh7Wrq7AIfICgYIARAAGAQSNwF-L9IrFZMq7f_9x6TSTIxs5wslbjy65HYOKDAlq6XuJkgzBXGgE5pu-lhi8jyrNCjLOpuVArc');
+        //     // $client->setAccessType('offline');
+        //     // $client->setApprovalPrompt('force');
+        //     // $service = new \Google_Service_Drive($client);
+        //     // $adapter = new GoogleDriveAdapter($service, '1zl3DzbRCPNbLXu2uN_MEWL1PHB1qWMSK');
+
+        //     // return new Filesystem($adapter);
         });
     }
 }
---
+++ config/values.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'GOOGLE_CLIENT_ID' => env('GOOGLE_CLIENT_ID', null),
+    'GOOGLE_CLIENT_SECRET' => env('GOOGLE_CLIENT_SECRET', null),
+    'GOOGLE_REFRESH_TOKEN' => env('GOOGLE_REFRESH_TOKEN', null),
+    'GOOGLE_DRIVE_FOLDER_ID' => env('GOOGLE_DRIVE_FOLDER_ID', null),
+
+    //add other values as you want
+
+];
